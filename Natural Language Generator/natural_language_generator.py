f8fc6f74fd4026f75a09f810916e24f39344e2c2
Evanl202 <evanlin202@gmail.com>
2026-09-19 11:13:03 -0400

    Update natural_language_generator.py

--- Natural Language Generator/natural_language_generator.py
+++ Natural Language Generator/natural_language_generator.py
@@ -74,11 +74,12 @@ metadata = {
 }
 
 #test function
-print(generate_intro(metadata))
-print(generate_enemy_dialogue(metadata))
-print(generate_dialogue(metadata, situations.ATTACK))
-print(generate_dialogue(metadata, situations.DAMAGE))
-print(generate_dialogue(metadata, situations.LOW_HEALTH))
-print(generate_dialogue(metadata, situations.DODGE))
-print(generate_dialogue(metadata, situations.VICTORY))
-print(generate_dialogue(metadata, situations.DEFEAT))
+if __name__ == "__main__":
+    print(generate_intro(metadata))
+    print(generate_enemy_dialogue(metadata))
+    print(generate_dialogue(metadata, situations.ATTACK))
+    print(generate_dialogue(metadata, situations.DAMAGE))
+    print(generate_dialogue(metadata, situations.LOW_HEALTH))
+    print(generate_dialogue(metadata, situations.DODGE))
+    print(generate_dialogue(metadata, situations.VICTORY))
+    print(generate_dialogue(metadata, situations.DEFEAT))
